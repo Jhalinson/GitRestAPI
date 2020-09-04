bc43c14d646ad441a2f17edc6d45642ce588ff5d
Jhalinson Acosta Mercedes <jhalinson@hotmail.com>
2020-09-04 17:24:33 -0400

    add comments

--- src/main/java/bdd/APIFramework/App.java
+++ src/main/java/bdd/APIFramework/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+// add some comments
+    	System.out.println( "Hello World!" );
     }
 }
